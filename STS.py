0ced4884d68b4c5aab5bcb6424e30aec30a057c3
asopos <rene.lehmann@stud.uni-due.de>
2018-10-14 16:47:21 +0200

    Adding Embeddings

--- STS.py
+++ STS.py
@@ -4,14 +4,28 @@ sentences_en = []
 sentences_es = []
 
 
+embeddings_words = []
+words_en = []
 with open('STS2017.eval.v1.1/STS.input.track4a.es-en.txt') as f:
     for sentence_pair in f.read().splitlines():
         sentence_es, sentence_en = sentence_pair.split('\t')
         sentences_en.append(sentence_en)
+        print(sentence_en)
         sentences_es.append(sentence_es)
 
-for sentence_en in sentences_en:
-    print(sentence_en)
+with open('/Users/renelehmann/Documents/Semantic-Textual-Similarity/MWE/english.txt') as f:
+    for line in f:
+        line = line.split()
+#        if line[0] in sentence_en and len(line[0]) > 2:
+        embeddings_words.append(line[0])
+for sentence in sentences_en:
+    for word in sentence.split():
+        if word in embeddings_words:
+            print(word)
+
+
+print(len(embeddings_words))
+
 
 def avg_sentence_vector(words, model, num_features, index2word_set):
     #function to average all words vectors in a given paragraph
