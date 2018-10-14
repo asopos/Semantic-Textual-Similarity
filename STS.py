7fee3886f71e807f7ecdab2cfd1990a375fa1ebe
asopos <rene.lehmann@stud.uni-due.de>
2018-10-14 14:22:59 +0200

    Load sentences from evaluation

--- STS.py
+++ STS.py
@@ -1,5 +1,17 @@
 
 import numpy as np
+sentences_en = []
+sentences_es = []
+
+
+with open('STS2017.eval.v1.1/STS.input.track4a.es-en.txt') as f:
+    for sentence_pair in f.read().splitlines():
+        sentence_es, sentence_en = sentence_pair.split('\t')
+        sentences_en.append(sentence_en)
+        sentences_es.append(sentence_es)
+
+for sentence_en in sentences_en:
+    print(sentence_en)
 
 def avg_sentence_vector(words, model, num_features, index2word_set):
     #function to average all words vectors in a given paragraph
