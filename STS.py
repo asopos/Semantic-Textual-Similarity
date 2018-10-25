9752a7b8265f35a6940a531c0c53b7c041c5f923
asopose <rene.lehmann@stud.uni-due.de>
2018-10-25 11:38:56 +0200

    efficient gold data loading

--- STS.py
+++ STS.py
@@ -1,24 +1,22 @@
 
+import io
 import numpy as np
-sentences_en = []
-sentences_es = []
+sentences = []
 
 
 embeddings_words = []
 words_en = []
-with open('STS2017.eval.v1.1/STS.input.track4a.es-en.txt') as f:
-    for sentence_pair in f.read().splitlines():
+with open('STS2017.eval.v1.1/STS.input.track4a.es-en.txt') as f, open('STS2017.gs/STS.gs.track4a.es-en.txt') as g:
+    for sentence_pair, gold_value in zip(f.read().splitlines(), g.read().splitlines()):
         sentence_es, sentence_en = sentence_pair.split('\t')
-        sentences_en.append(sentence_en)
-        print(sentence_en)
-        sentences_es.append(sentence_es)
-
+        sentences.append((sentence_en,sentence_es, gold_value))
+    
 with open('/Users/renelehmann/Documents/Semantic-Textual-Similarity/MWE/english.txt') as f:
     for line in f:
         line = line.split()
 #        if line[0] in sentence_en and len(line[0]) > 2:
         embeddings_words.append(line[0])
-for sentence in sentences_en:
+for sentence in sentences:
     for word in sentence.split():
         if word in embeddings_words:
             print(word)
