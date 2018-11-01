c1b8d183e434e985969337184b8458aeb48c5a7a
asopos <rene.lehmann@stud.uni-due.de>
2018-11-01 19:36:23 +0100

    save word embeddings

--- STS.py
+++ STS.py
@@ -1,28 +1,45 @@
 
 import io
 import numpy as np
+from nltk.tokenize import word_tokenize
 sentences = []
 
 
-embeddings_words = []
-words_en = []
+embeddings_words = {}
 with open('STS2017.eval.v1.1/STS.input.track4a.es-en.txt') as f, open('STS2017.gs/STS.gs.track4a.es-en.txt') as g:
     for sentence_pair, gold_value in zip(f.read().splitlines(), g.read().splitlines()):
         sentence_es, sentence_en = sentence_pair.split('\t')
         sentences.append((sentence_en,sentence_es, gold_value))
-    
-with open('/Users/renelehmann/Documents/Semantic-Textual-Similarity/MWE/english.txt') as f:
-    for line in f:
-        line = line.split()
-#        if line[0] in sentence_en and len(line[0]) > 2:
-        embeddings_words.append(line[0])
-for sentence in sentences:
-    for word in sentence.split():
-        if word in embeddings_words:
-            print(word)
 
+sentence_token = [word_tokenize(sentence[0]) for sentence in sentences]
+# with open('/Users/renelehmann/Documents/Semantic-Textual-Similarity/MWE/english.txt') as f:
+#     for line in f:
+#         line = line.split()
+#         for tokens in sentence_token:
+#             if line[0] in tokens:
+#                 embeddings_words[line[0]] = line[1:]
+#                 print(line[0])
+#                 break
 
-print(len(embeddings_words))
+def write_to_file(path, embeddings):
+    with open(path,'w') as f:
+        for embedding in embeddings:
+            f.write(embedding)
+    return True
+
+def save_embeddings(source_path, target_path, sen_Token):
+    with open(source_path) as s, open(target_path,'w') as t:
+        for line in s:
+            word = line.split()
+            for tokens in sen_Token:
+                if word[0] in tokens:
+                    t.write(line)
+                    break
+
+save_embeddings(
+'/Users/renelehmann/Documents/Semantic-Textual-Similarity/MWE/english.txt',
+'/Users/renelehmann/Documents/Semantic-Textual-Similarity/MWE/english_new.txt',
+sentence_token)
 
 
 def avg_sentence_vector(words, model, num_features, index2word_set):
