2b737d73ed6495ed6a8b837a76acd7afdecc3c0a
asopos <rene.lehmann@stud.uni-due.de>
2018-11-07 18:04:19 +0100

    adding evaluation + embedding/word ratio

--- STS.py
+++ STS.py
@@ -5,6 +5,7 @@ from scipy import spatial
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import embedding as emb
+import evaluation as ev
 sentences = []
 
 embeddings_words = {}
@@ -21,8 +22,10 @@ word_es_embeddings = emb.get_word_embeddings('MWE/spanish_new2.txt')
 
 with open('predict.txt','w') as p:
     for en, es in zip(sentence_en_token, sentence_es_token):
-        s_vector_en = emb.sentence_embedding_avg(word_en_embeddings, en ,300)
-        s_vector_es = emb.sentence_embedding_avg(word_es_embeddings, es , 300)
+        s_vector_en, avr_vec_found_en = emb.sentence_embedding_avg(word_en_embeddings, en ,300)
+        s_vector_es, avr_vec_found_es = emb.sentence_embedding_avg(word_es_embeddings, es , 300)
         result = 1 - spatial.distance.cosine(s_vector_en, s_vector_es)
-        p.write(str(result) + '\n')
+        p.write(str(round(result*5,1))+ '\t'+ str(avr_vec_found_en) + '\t'+ str(avr_vec_found_es) + '\n')
         # print(en, ' ' , es , ': ' ,result)
+
+#print(ev.evaluationB('predict.txt','STS2017.gs/STS.gs.track4a.es-en.txt'))
