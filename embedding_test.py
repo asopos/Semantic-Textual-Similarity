5bce0a1fa966db1b073eb61c4fa26f13742d2ae6
asopose <rene.lehmann@stud.uni-due.de>
2018-12-06 00:04:35 +0100

    test for save embeddings

--- embedding_test.py
+++ embedding_test.py
@@ -1,5 +1,23 @@
-import unittest
-import embedding as emb
-import numpy as np
+relevant_missing_words = []
+google_missing_words =[]
 
-word_embeddings = emb.get_word_embeddings('MWE/english_new.txt')
+diffrences=[]
+
+with open('missing_token.txt', encoding='utf-8') as s:
+    for line in s:
+        relevant_missing_words.append(str(line))
+
+with open('missing_token_all_G.txt', encoding='utf-8') as s:
+    for line in s:
+        google_missing_words.append(str(line))
+
+for word in relevant_missing_words:
+    if word not in google_missing_words:
+        diffrences.append(word)
+
+
+print(len(diffrences))
+print(diffrences)
+
+
+print(len(relevant_missing_words),len(google_missing_words))
