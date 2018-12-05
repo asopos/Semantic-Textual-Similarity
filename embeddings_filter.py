8710daaa5077c48d7ff068b46515f0018a9a6a8f
asopose <rene.lehmann@stud.uni-due.de>
2018-12-06 00:05:16 +0100

    Add some testing methods

--- embeddings_filter.py
+++ embeddings_filter.py
@@ -23,10 +23,14 @@ def get_word_embeddings(path):
                 temp_embeddings[line[0]] = np.asarray(line[1:], dtype=np.float32)
     return temp_embeddings
 
-def get_word_embeddings_for_sentence(source_path,target_path, sentences):
+
+
+sentences = dev_data['sentence_1'].tolist() + dev_data['sentence_2'].tolist()
+
+def save_relevant_embeddings(source_path, target_path, sen_Token):
     word_list=[]
     count=0
-    for sentence in sentences:
+    for sentence in sen_Token:
         for word in sentence:
             if word not in word_list:
                 word_list.append(word)
@@ -39,6 +43,33 @@ def get_word_embeddings_for_sentence(source_path,target_path, sentences):
                 count+=1
     print(count)
 
-sentences = dev_data['sentence_1'].tolist() + dev_data['sentence_2'].tolist()
+def unique_words(sentence_list):
+    word_list=[]
+    for sentence in sentence_list:
+            for word in sentence:
+                if word not in word_list:
+                    word_list.append(word)
+    return word_list
 
-get_word_embeddings_for_sentence('C:\\Users\\Rene\\Documents\\Semantic Textual Similarity\\Word Embeddings\\dev_data_embeddings.txt','C:\\Users\\Rene\\Documents\\Semantic Textual Similarity\\Word Embeddings\\dev_data_embeddings_test.txt',sentences)
+def sentences_to_words(sentence_list):
+    word_list=[]
+    for sentence in sentence_list:
+        for word in sentence:
+            word_list.append(word)
+    return word_list
+
+dev_emb = get_word_embeddings('Word Embeddings\\dev_data_embeddings.txt')
+google_emb = get_word_embeddings('Word Embeddings\\GoogleNews-vectors-negative300.txt')
+
+def test_embeddings(emb, sentences):
+    words_without_embbedings=[]
+    word_list=sentences_to_words(sentences)
+    for word in word_list:
+        try:
+            a=emb[word]
+        except KeyError:
+            words_without_embbedings.append(word)
+    return words_without_embbedings
+
+
+print(len(test_embeddings(dev_emb,sentences)),len(test_embeddings(google_emb,sentences)))
