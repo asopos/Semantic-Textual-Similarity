a913d6e6e552abd4a99c76217fd72dd27df0c301
asopose <rene.lehmann@stud.uni-due.de>
2018-12-06 00:04:08 +0100

    change save embeddings

--- embedding.py
+++ embedding.py
@@ -1,25 +1,31 @@
 import numpy as np
 from nltk.tag import pos_tag
+from gensim import matutils
 
 def get_word_embeddings(path):
     temp_embeddings={}
-    with open(path) as f:
+    with open(path, encoding='utf-8') as f:
         for line in f:
             line = line.split()
-            try:
-                temp_embeddings[line[0]] = np.asarray(line[1:], dtype=np.float32)
-            except ValueError:
-                temp_embeddings[line[1]] = np.asarray(line[2:], dtype=np.float32)
+            if len(line)>2:
+                temp_embeddings[line[0].lower()] = np.asarray(line[1:], dtype=np.float32)
     return temp_embeddings
 
 def save_relevant_embeddings(source_path, target_path, sen_Token):
-    with open(source_path, encoding='utf-8') as s, open(target_path,'w', encoding='utf-8') as t:
+    word_list=[]
+    count=0
+    for sentence in sen_Token:
+        for word in sentence:
+            if word not in word_list:
+                word_list.append(word)
+    with open(source_path, encoding='utf-8') as s, open(target_path,'w',encoding='utf-8') as f:
         for line in s:
-            word = line.split()
-            for tokens in sen_Token:
-                if word[0] in tokens:
-                    t.write(line)
-                    break
+            sline = line.split()
+            if sline[0] in word_list:
+                f.write(line)
+            else:
+                count+=1
+    print(count)
 
 def sentence_embedding_avg(word_embeddings, tokens,dimension):
     sentence_vector = np.zeros(dimension, dtype="float32")
@@ -53,3 +59,13 @@ def sentence_embedding_max(word_embeddings, tokens):
         if word in tokens:
             temp_word_embeddings.append(word_embeddings[word])
     return np.asarray(temp_word_embeddings).max(0)
+
+def sentence_similarity(emb,sentence_1,sentence_2):
+    v1 = []
+    v2 = []
+    try:
+        v1 = [emb[word.lower()] for word in sentence_1]
+        v2 = [emb[word.lower()] for word in sentence_2]
+    except KeyError as error:
+        print(error)
+    return np.dot(matutils.unitvec(np.array(v1).mean(axis=0)), matutils.unitvec(np.array(v2).mean(axis=0)))
