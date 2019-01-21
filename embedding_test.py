28e1d426b231c01a16fa1125b4d4a9832ea9fdd9
asopose <rene.lehmann@stud.uni-due.de>
2019-01-21 12:07:29 +0100

    delete some exampels

--- embedding_test.py
+++
@@ -1,9 +0,0 @@
-from gensim.models import FastText
-
-fastText_emb = FastText.load_fasttext_format('Word Embeddings\\fastText_Skipgram_WIki.bin', 'utf-8')
-
-sent_1 =['Hello','i','AM','obama']
-
-sent_2 = ['I','am', 'the', 'President', 'of', 'the', 'usa']
-
-print(fastText_emb['o'])
